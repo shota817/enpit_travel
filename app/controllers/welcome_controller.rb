486e7fc6fc9540572c1c051c7cc36a4f181667e0
Yoshifumi Matsumoto <matsumo1001@users.noreply.github.com>
2014-09-26 08:14:49 +0000

    Update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,19 @@
+require 'rakuten_web_service'
+
 class WelcomeController < ApplicationController
-  def show
+  def index
+    @items
+  end
+  def test
     render :text => "検索商品名 = #{params[:item]}"
+  end
+  def show
+    RakutenWebService.configuration do |c|
+      c.application_id = ENV["APPID"]
+      c.affiliate_id = ENV["AFID"]
+    end
+    keyword = params[:item]
+    @items = RakutenWebService::Ichiba::Item.search(:keyword => keyword, :hits => 10, :sort => '-itemPrice') # This returns Enamerable object
+    render :action => 'item_result.erb'
   end
 end
